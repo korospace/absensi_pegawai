8fb7cf38cca463aacbda44d29a8ab3eca6c214a8
korospace <bagaselkoro@gmail.com>
2023-05-28 20:05:27 +0700

    change base url in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/absensi_pegawai_daniel/';
+$config['base_url'] = 'http://localhost/absensi_pegawai/';
 
 /*
 |--------------------------------------------------------------------------
